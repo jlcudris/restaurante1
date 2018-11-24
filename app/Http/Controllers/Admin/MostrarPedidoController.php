9d78602c53870b41f0de93a2cc639dc4a46924f2
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2018-11-24 18:17:02 -0500

    Listado de pedidos
    ->lista los pedidos que estan pendientes en la fecha actual

--- app/Http/Controllers/Admin/MostrarPedidoController.php
+++ app/Http/Controllers/Admin/MostrarPedidoController.php
@@ -8,18 +8,28 @@ use Illuminate\Support\Facades\DB;
 
 class MostrarPedidoController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         //iterar todos los pedidos que estan pendiente y listarlos por fecha 
-
-        $pedido = DB::table('detallespedidos_platos as dp')
-        ->select('pl.nombre', 'pl.imagenplato', 'pe.nombre_cliente', 'me.num_mesa', 'pe.fecha_pedido', 'pe.hora_pedido', 'pe.estado')
-        ->join('platos as pl', 'dp.id_plato','pl.id_plato')
-        ->join('pedido as pe', 'dp.id_pedido','pe.id_pedido')
-        ->join('mesas as me', 'pe.id_mesa','me.id')
-        ->where('dp.id_pedido',request('id_pedido'))
+        $pedidos = DB::table('pedido as pe')
+        ->where('pe.estado','pendiente')
+        ->where('pe.fecha_pedido',date('Y-m-d'))
         ->get();
+
+
+        $pedidos_arr = array();
+        foreach($pedidos as $pedido){
+            $lista_pedido = DB::table('detallespedidos_platos as dp')
+            ->select('pl.nombre', 'pl.imagenplato', 'pe.nombre_cliente', 'me.num_mesa', 'pe.fecha_pedido', 'pe.hora_pedido', 'pe.estado')
+            ->join('platos as pl', 'dp.id_plato','pl.id_plato')
+            ->join('pedido as pe', 'dp.id_pedido','pe.id_pedido')
+            ->join('mesas as me', 'pe.id_mesa','me.id')
+            ->where('dp.id_pedido',$pedido->id_pedido)
+            ->get();
+
+            $pedidos_arr = array_add($pedidos_arr, $pedido->id_pedido, $lista_pedido);
+        }
         
-        return response()->json([$pedido]);
+        return response()->json($pedidos_arr);
     }
 }
